76883a39f5df023d46b91ad2ee3c84c37a98fd07
egunde <ethangunderson3@gmail.com>
2022-11-09 16:28:16 -0600

    Added Name

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -6,40 +6,40 @@ namespace GithubActionsLab
     [TestFixture]
     public class Math
     {
-        [TestGunderson]
-        public void Add_Valid()
+        [Test]
+        public void Add_Valid_Gunderson()
         {
             Assert.AreEqual(3, Program.Add("1", "2"));
             Assert.AreEqual(5, Program.Add("3", "2"));
             Assert.AreEqual(12, Program.Add("5", "7"));
         }
 
-        [TestGunderson]
-        public void Add_Invalid()
+        [Test]
+        public void Add_Invalid_Gunderson()
         {
             Assert.Throws<FormatException>(() => Program.Add("1", "a"));
             Assert.Throws<FormatException>(() => Program.Add("a", "1"));
             Assert.Throws<FormatException>(() => Program.Add("a", "a"));
         }
 
-        [TestGunderson]
-        public void Add_Null()
+        [Test]
+        public void Add_Null_Gunderson()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
         }
         
-        [TestGunderson]
-        public void Subtract_Valid()
+        [Test]
+        public void Subtract_Valid_Gunderson()
         {
             Assert.AreEqual(1, Program.Subtract("3","2"));
             Assert.AreEqual(8, Program.Subtract("13", "5"));
             Assert.AreEqual(9, Program.Subtract("15", "6"));
         }
 
-        [TestGunderson]
-        public void Subtract_Invalid()
+        [Test]
+        public void Subtract_Invalid_Gunderson()
         {
             Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
             Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
@@ -47,56 +47,56 @@ namespace GithubActionsLab
 
         }
 
-        [TestGunderson]
-        public void Subtract_Null()
+        [Test]
+        public void Subtract_Null_Gunderson()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
             Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
         }
 
-        [TestGunderson]
-        public void Multiply_Valid()
+        [Test]
+        public void Multiply_Valid_Gunderson()
         {
             Assert.AreEqual(4, Program.Multiply("1","4"));
             Assert.AreEqual(12, Program.Multiply("3", "4"));
             Assert.AreEqual(35, Program.Multiply("5", "7"));
         }
 
-        [TestGunderson]
-        public void Multiply_Invalid()
+        [Test]
+        public void Multiply_Invalid_Gunderson()
         {
             Assert.Throws<FormatException>(() => Program.Multiply("1", "a"));
             Assert.Throws<FormatException>(() => Program.Multiply("a", "1"));
             Assert.Throws<FormatException>(() => Program.Multiply("a", "a"));
         }
 
-        [TestGunderson]
-        public void Multiply_Null()
+        [Test]
+        public void Multiply_Null_Gunderson()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "1"));
             Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, null));
         }
 
-        [TestGunderson]
-        public void Divide_Valid()
+        [Test]
+        public void Divide_Valid_Gunderson()
         {
             Assert.AreEqual(3, Program.Divide("3", "1"));
             Assert.AreEqual(4, Program.Divide("12", "3"));
             Assert.AreEqual(5, Program.Divide("20", "4"));
         }
 
-        [TestGunderson]
-        public void Divide_Invalid()
+        [Test]
+        public void Divide_Invalid_Gunderson()
         {
             Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
             Assert.Throws<FormatException>(() => Program.Divide("a", "1"));
             Assert.Throws<FormatException>(() => Program.Divide("a", "a"));
         }
 
-        [TestGunderson]
-        public void Divide_Null()
+        [Test]
+        public void Divide_Null_Gunderson()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
